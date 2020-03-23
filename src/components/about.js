58fdc92af7ab3d28e61d876b5ff7bb664ce00d20
Martin Gras <martin.gras95@gmail.com>
2020-03-23 00:48:31 +0000

    testimonials and about section merged

--- src/components/about.js
+++ src/components/about.js
@@ -6,6 +6,12 @@ import { faQuestion } from '@fortawesome/free-solid-svg-icons'
 import { faHeadset } from '@fortawesome/free-solid-svg-icons'
 import { faExclamation } from '@fortawesome/free-solid-svg-icons'
 
+import { Carousel } from 'react-bootstrap'
+
+import slide01 from "../images/slide.jpg"
+import slide02 from "../images/slide.jpg"
+import slide03 from "../images/slide.jpg"
+
 const questionIcon = <FontAwesomeIcon icon={faQuestion} size="4x" />
 const headsetIcon = <FontAwesomeIcon icon={faHeadset} size="4x" />
 const actionIcon = <FontAwesomeIcon icon={faExclamation} size="4x" />
@@ -33,6 +39,41 @@ const About = () => {
                     Take Action!
                 </div>
             </div>
+
+            <Carousel controls={false} indicators={false} interval={5000}>
+                    <Carousel.Item>
+                        <img 
+                        className={`d-block w-100 ${ aboutStyles.slide }`}
+                        src={slide01} alt="first slide" 
+                        />
+                        <Carousel.Caption style={{color:"#212529"}}>
+                            <h3>“Great Advice!”</h3>
+                            <p>I was given great tips of marketing and sales!</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img 
+                        className={`d-block w-100 ${ aboutStyles.slide }`}
+                        src={slide02} alt="second slide" 
+                        />
+                        <Carousel.Caption style={{color:"black"}}>
+                            <h3>“Recommend 100%”</h3>
+                            <p>Matt, one of the advisors was kind and helpful :)</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item >
+                        <img 
+                        className={`d-block w-100 ${ aboutStyles.slide }`}
+                        src={slide03} alt="third slide" 
+                        />
+                        <Carousel.Caption style={{color:"black"}}>
+                            <h3>“Easy process”</h3>
+                            <p>It was very easy to find all the information I needed!</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+
+
         </div>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import React from "react"
 import Layout from "../components/layout"
 import Jumbo from "../components/jumbotron"
 import About from "../components/about"
-import Testimonials from "../components/testimonials"
 import Footer from "../components/footer"
 
 import "../styles/global.css"
@@ -12,10 +11,8 @@ import "../styles/global.css"
 const IndexPage = () => {
     return (
         <Layout>
-            
             <Jumbo />
             <About />
-            <Testimonials />
             <Footer />
         </Layout>
     )
